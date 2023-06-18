d9c54309fa617e2293907592b2638ecba0792441
M3li55aH <melissa.hammaoui@edu.univ-paris13.fr>
2023-06-18 22:27:18 +0200

    Affichage traduction(s) d'un mot

--- dict/src/Controller/ServiceController.php
+++ dict/src/Controller/ServiceController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class ServiceController extends AbstractController
 {
@@ -34,6 +35,18 @@ class ServiceController extends AbstractController
         return null;
     }
 
+    private function getMotspecifique($motId): ?string
+    {
+        $responseWord = $this->apiClientWord->request('GET', 'http://127.0.0.1:8000/api/words/'.$motId);
+
+
+        if ($responseWord->getStatusCode() === 200 ) {
+            return $responseWord->getContent();
+        }
+
+        return null;
+    }
+
     private function DownloadLanguageName($languageId): ?string
     {
         $responseLanguage = $this->apiClientLanguage->request('GET', 'http://127.0.0.1:8000/api/languages/'.$languageId);
@@ -48,6 +61,83 @@ class ServiceController extends AbstractController
         return null;
     }
 
+    private function GetTraductionWord($traductionId): ?string
+    {
+        $responseLanguage = $this->apiClientLanguage->request('GET', 'http://127.0.0.1:8000/api/traductions/'.$traductionId);
+
+
+        if ($responseLanguage->getStatusCode() === 200 ) {
+            return $responseLanguage->getContent();
+        }
+
+        return null;
+    }
+
+    function extractIdUrl($regexp, $text) {
+        if (preg_match($regexp, $text, $matches)) {
+            if (isset($matches[1])) {
+                return $matches[1];
+            }
+        }
+        // Retourne null si aucun ID n'est trouvé
+        return null;
+    }
+
+    #[Route('/showWord', name:'showWord', methods:['GET'])]
+    public function showWord(Request $request): Response
+    {
+        // récuperation de id du mot
+        $wordId = $request->query->get('id');
+
+        // partie affichage du mot spécifique à partir de l'url de l'api
+        $dictionary = $this->getMotspecifique($wordId);
+        $wordsArray = json_decode($dictionary, true);
+
+        if (is_array($wordsArray) && count($wordsArray) > 0) {
+            $definition = $wordsArray['def'];
+            $inputWord = $wordsArray['inputWord'];
+            $wordType = $wordsArray['wordType'];
+            $languageId = $wordsArray['language'];
+            $traductionId = $wordsArray['traductions'];
+
+            $regexpL = '/\/api\/languages\/(\d+)/';
+            $idL = $this->extractIdUrl($regexpL, $languageId);
+
+
+            $ids = [];
+            foreach ($traductionId as $traduction) {
+                $regexpT = '/\/api\/traductions\/(\d+)/';
+                $idT =  $this->extractIdUrl($regexpT, $traduction);
+                $ids[] = $idT;
+
+            }
+
+            $languageName = $this->DownloadLanguageName($idL);
+
+            $outputField = "Mot : $inputWord<br>";
+            $outputField .= "Définition : $definition<br>";
+            $outputField .= "Type du mot : $wordType<br>";
+            $outputField .= "Langue : $languageName<br>";
+
+
+            foreach ($ids as $id) {
+                $traductionName = $this->GetTraductionWord($id);
+                $data = json_decode($traductionName);
+                $outputField .= "traduction du word : $data->translatedWord.<br>";
+                $outputField .= "traduction definition : $data->def.<br>";
+
+                $idLTrad = $this->extractIdUrl($regexpL, $data->languageTrad);
+                $languageTrad = $this->DownloadLanguageName($idLTrad);
+                $outputField .= "traduction langue : $languageTrad.<br>";
+            }
+
+        } else {
+            $outputField = 'No word found';
+        }
+
+
+        return new Response($outputField, 200, ['Content-Type' => 'text/html']);
+    }
 
 
     #[Route('/randomWordService', name:'randomWord', methods:['GET'])]
@@ -84,9 +174,13 @@ class ServiceController extends AbstractController
         return new Response($outputField, 200, ['Content-Type' => 'text/html']);
     }
 
+
+
+
     #[Route('/service', name: 'app_service')]
     public function index(): Response
     {
+
         return $this->render('service/index.html.twig', [
             'controller_name' => 'ServiceController',
         ]);
